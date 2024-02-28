e4179e02bc2b1fa71f0bd8788d000e3cc9d74ed0
farhanazulaikha <farhanazulaikha499@gmail.com>
2024-02-28 23:24:58 +0800

    add welcome and sign out button to navbar

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,11 +1,13 @@
 import './navbar.css'
 
 export default function Navbar() {
-
-
     return(
         <div className="navbar">
-            <div className="right-section">
+            <div className="navbar__right-section">
+                <div>Welcome, xxx</div>
+                <div className="navbar__right-section__button-div">
+                    <button className="navbar__right-section__button">Sign out</button>
+                </div>
             </div>
         </div>
     )
