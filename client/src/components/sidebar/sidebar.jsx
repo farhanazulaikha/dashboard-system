260459b372c81bf95d86f7108d1e26b38c61795c
farhanazulaikha <farhanazulaikha499@gmail.com>
2024-02-29 15:42:42 +0800

    get filter value

--- client/src/components/sidebar/sidebar.jsx
+++ client/src/components/sidebar/sidebar.jsx
@@ -1,17 +1,41 @@
+import { useState } from 'react'
+import React from 'react';
 import './sidebar.css'
 
 export default function Sidebar() {
+
+    const [filter, setFilter] = useState({
+        filterOne: "",
+        filterTwo: ""
+    });
+
+    const handleFilter = (e) => {
+        const { name, value } = e.target;
+
+        setFilter(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
+    }
+
+    const handleApply = (e) => {
+        e.preventDefault();
+        console.log(filter);
+    }
+
     return(
         <div className="sidebar">
             <div className="sidebar__element">
                 <div className="sidebar__element-input-div">
-                    <input type="text" placeholder="Filter 1" className="sidebar__element-input"></input>
+                    <input type="text" placeholder="Filter 1" className="sidebar__element-input" 
+                    value={filter["filterOne"]} onChange={handleFilter} name="filterOne"></input>
                 </div>
                 <div className="sidebar__element-input-div">
-                    <input type="text" placeholder="Filter 2" className="sidebar__element-input"></input>
+                    <input type="text" placeholder="Filter 2" className="sidebar__element-input" 
+                    value={filter["filterTwo"]} onChange={handleFilter} name="filterTwo"></input>
                 </div>
                 <div className="sidebar__element-button-div">
-                    <button className="sidebar__element-button">Apply</button>
+                    <button className="sidebar__element-button" type="submit" onClick={handleApply}>Apply</button>
                 </div>
             </div>
         </div>
