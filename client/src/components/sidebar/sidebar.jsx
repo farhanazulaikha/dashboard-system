791cde9ceb0a385a1e1067eeccb6a5890a24ecb3
farhanazulaikha <farhanazulaikha499@gmail.com>
2024-02-29 15:10:20 +0800

    add sidebar input and button UI

--- client/src/components/sidebar/sidebar.jsx
+++ client/src/components/sidebar/sidebar.jsx
@@ -3,7 +3,17 @@ import './sidebar.css'
 export default function Sidebar() {
     return(
         <div className="sidebar">
-            
+            <div className="sidebar__element">
+                <div className="sidebar__element-input-div">
+                    <input type="text" placeholder="Filter 1" className="sidebar__element-input"></input>
+                </div>
+                <div className="sidebar__element-input-div">
+                    <input type="text" placeholder="Filter 2" className="sidebar__element-input"></input>
+                </div>
+                <div className="sidebar__element-button-div">
+                    <button className="sidebar__element-button">Apply</button>
+                </div>
+            </div>
         </div>
     )
 }
