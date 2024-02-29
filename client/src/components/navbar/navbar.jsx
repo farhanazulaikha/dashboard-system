9d85f11acafdaa180d5e146eb06db97eb5d323b3
farhanazulaikha <farhanazulaikha499@gmail.com>
2024-02-29 15:12:10 +0800

    bold name in welcome message

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar({name}) {
     return(
         <div className="navbar">
             <div className="navbar__right-section">
-                <div>Welcome, {name}</div>
+                <div>Welcome, <span>{name}</span></div>
                 <div className="navbar__right-section__button-div">
                     <button className="navbar__right-section__button">Sign out</button>
                 </div>
